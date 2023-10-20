55776cda0320ad1d3cfb7dd7fb24e76eb5a3c0e7
DaBuggins <109602512+DaBuggins@users.noreply.github.com>
2023-10-20 08:00:37 +0100

    Create ShoppingListForm.jsx
    Main form of shopping list app

---
+++ src/ShoppingListForm.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+export default function ShoppingListForm() {
+  const [formData, setFormData] = useState({ product: "", quantity: 0 });
+  const handleChange = (evt) => {
+    setFormData((currData) => {
+      return {
+        ...currData,
+        [evt.target.name]: evt.target.value
+      };
+    });
+  };
+  return (
+    <form>
+      <h1>Product is: {formData.product}</h1>
+      <label htmlFor="product">Product Name</label>
+      <input
+        type="text"
+        placeholder="product name"
+        name="product"
+        id="product"
+        onChange={handleChange}
+        value={formData.product}
+      />
+    </form>
+  );
+}
